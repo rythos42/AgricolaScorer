c261f21203f7ab26a4cb7dcc1012665f466e5f59
Craig Fleming <rythos42@gmail.com>
2013-06-10 15:01:35 -0700

    Re-styling action overflow button, adding black background to settings

--- src/com/geeksong/agricolascorer/SettingsActivity.java
+++ src/com/geeksong/agricolascorer/SettingsActivity.java
@@ -9,6 +9,7 @@ public class SettingsActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
 
         ActionBarHelper.setActionBarTitle(this, R.string.settings);
 		
