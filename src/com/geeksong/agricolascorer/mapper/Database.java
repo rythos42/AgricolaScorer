e57ba44c43f7dda0eeeb5947476b0097e3ab15a5
Craig Fleming <rythos42@gmail.com>
2013-06-17 16:33:47 -0700

    Adding a column to the database for future work

--- src/com/geeksong/agricolascorer/mapper/Database.java
+++ src/com/geeksong/agricolascorer/mapper/Database.java
@@ -7,10 +7,11 @@ import com.geeksong.agricolascorer.model.GameType;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class Database extends SQLiteOpenHelper {
 	private static final String Name = "AgricolaScorer";
-	private static final int Version = 26;
+	private static final int Version = 31;
 	
 	
     public static final String TABLE_RECENTPLAYERS = "RecentPlayers";
@@ -22,6 +23,7 @@ public class Database extends SQLiteOpenHelper {
     public static final String KEY_NAME = "name";
     public static final String KEY_FARMERS = "farmers";
     public static final String KEY_GAMECOUNT = "gameCount";
+    public static final String KEY_WHO_WON = "whoWon";
 	
 	public static final String KEY_DATE = "playedDate";
 	public static final String KEY_GAME_TYPE = "gameType";
@@ -85,7 +87,8 @@ public class Database extends SQLiteOpenHelper {
     	String CREATE_GAMES_TABLE = "CREATE TABLE " + TABLE_GAMES + "(" +
 	    		KEY_ID + " INTEGER PRIMARY KEY, " +
 	    		KEY_DATE + " INTEGER, " +
-	    		KEY_GAME_TYPE + " INTEGER " + 
+	    		KEY_GAME_TYPE + " INTEGER, " +
+	    		KEY_WHO_WON + " INTEGER " +
 			")";
     	db.execSQL(CREATE_GAMES_TABLE);
         	
@@ -142,25 +145,26 @@ public class Database extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     	// KEY_GAMECOUNT on TABLE_RECENTPLAYERS isn't used anymore and could be dropped if SQLite supported it nicely.
     	// "farmers" on TABLE_GAMES can be dropped.
-	    
+    	
     	if(oldVersion <= 19) {
 	    	db.execSQL("ALTER TABLE " + TABLE_SCORES + " ADD COLUMN " + KEY_HORSESCORE + " INTEGER");
 	    	db.execSQL("ALTER TABLE " + TABLE_SCORES + " ADD COLUMN " + KEY_INBEDFAMILYCOUNT + " INTEGER");
 	    	db.execSQL("ALTER TABLE " + TABLE_SCORES + " ADD COLUMN " + KEY_TOTALFAMILYCOUNT + " INTEGER");
     	}
-   	
-    	if(oldVersion <= 25) {
+    	
+    	if(oldVersion <= 30) {
     		createAllCreaturesScoreTable(db);
 
     		db.execSQL("ALTER TABLE " + TABLE_GAMES + " ADD COLUMN " + KEY_GAME_TYPE + " INTEGER");
+			db.execSQL("ALTER TABLE " + TABLE_GAMES + " ADD COLUMN " + KEY_WHO_WON + " INTEGER");
 
     		// Make GameType column populated for all previous games.
     		db.execSQL(String.format(Locale.US, "UPDATE %s SET %s=%d WHERE %s=%d AND (%s IS NULL OR %s ='')", 
     				TABLE_GAMES, KEY_GAME_TYPE, GameType.Farmers.ordinal(), KEY_FARMERS, 1, KEY_GAME_TYPE, KEY_GAME_TYPE));
     		db.execSQL(String.format(Locale.US, "UPDATE %s SET %s=%d WHERE (%s=%d OR %s IS NULL) AND (%s IS NULL OR %s = '')", 
     				TABLE_GAMES, KEY_GAME_TYPE, GameType.Agricola.ordinal(), KEY_FARMERS, 0, KEY_FARMERS, KEY_GAME_TYPE, KEY_GAME_TYPE));
-    		
-    		db.execSQL("DROP TABLE " + TABLE_SETTINGS);
+    	
+			db.execSQL("DROP TABLE " + TABLE_SETTINGS);
     	}
     }
 }
